ef5c8bc41d84b4f1911eff008743f67cb3d96e0d
Ayush <hackerboy5556@gmail.com>
2024-12-12 00:26:25 +0530

    add all

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // Import the Vercel adapter
-import vercel from '@astrojs/vercel/serverless';
+// import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
+  // output: 'server',
+  // adapter: netlify(),
 });
